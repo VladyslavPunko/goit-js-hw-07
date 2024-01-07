5c047143c600205f2da9955af1751003f9963143
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-01-07 23:18:54 +0100

    tusk 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(`.login-form`);
 
+loginForm.addEventListener(`submit`, function (event) {
+	event.preventDefault();
+    
+    const emailValue = this.elements.email.value.trim();
+    const passwordValue = this.elements.password.value.trim();
+
+    let formData ={} ;
+
+    if (!emailValue || !passwordValue){
+        alert('All form fields must be filled in')
+    } else { formData = {
+            email: emailValue,
+            password: passwordValue,
+        };
+    }
+    console.log(formData);
+
+    this.reset();
+    
+});
