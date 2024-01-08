5ea4a56cfc56868215fe02db5e0bdc5f706b33b0
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-01-09 00:48:22 +0100

    6

--- js/task-6.js
+++ js/task-6.js
@@ -4,3 +4,43 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+function clearBoxesContainer() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+function createBoxes(amount) {
+  clearBoxesContainer(); // Очищення попередніх елементів
+
+  const boxesContainer = document.getElementById("boxes");
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = 30 + i * 10;
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.appendChild(box);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const createButton = document.querySelector("[data-create]");
+  const destroyButton = document.querySelector("[data-destroy]");
+  const input = document.querySelector("input");
+
+  createButton.addEventListener("click", () => {
+    const amount = parseInt(input.value);
+
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+      input.value = "";
+    }
+  });
+
+  destroyButton.addEventListener("click", () => {
+    clearBoxesContainer();
+  });
+});
